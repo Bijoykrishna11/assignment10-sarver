e6711fbb72cfde417c7d7138eb59f7bc05425224
Bijoy <hskbijoy123@gmail.com>
2022-10-26 18:58:36 +0600

    added all books functiom

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const port = process.env.PORT || 5000;
 
 const books = require('./data/Catagories.json');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
-app.get('/books-catagories', (req, res) => {
-    res.send(books)
+app.get("/", (req, res) => {
+    res.send(books);
 })
+
 
 app.listen(port, () => {
     console.log('Example app listening on port', port);
