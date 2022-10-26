9e598c0558f5ab883667178a786d10298c16c9ed
Bijoy <hskbijoy123@gmail.com>
2022-10-26 19:46:47 +0600

    update cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
+const port = process.env.port || 5000;
 
 const books = require('./data/Catagories.json');
 
